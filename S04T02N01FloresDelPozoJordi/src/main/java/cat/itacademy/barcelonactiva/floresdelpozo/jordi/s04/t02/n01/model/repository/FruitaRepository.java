b5aa7fb6ccb8bcb8fd6627b09a7c251cf43c0f04
Jordi F <jordi_sntboi_@hotmail.com>
2023-06-19 15:20:52 +0200

    S04T02N01

---
+++ S04T02N01FloresDelPozoJordi/src/main/java/cat/itacademy/barcelonactiva/floresdelpozo/jordi/s04/t02/n01/model/repository/FruitaRepository.java
@@ -0,0 +1,8 @@
+package cat.itacademy.barcelonactiva.floresdelpozo.jordi.s04.t02.n01.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import cat.itacademy.barcelonactiva.floresdelpozo.jordi.s04.t02.n01.model.domain.Fruita;
+
+public interface FruitaRepository extends JpaRepository<Fruita, Integer>{
+
+}

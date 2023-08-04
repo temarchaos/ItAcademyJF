a7cfe0b457f1b8aa91611202b75018a3e7e5aa77
Jordi F <jordi_sntboi_@hotmail.com>
2023-08-04 12:49:06 +0200

    PlayerRepository

---
+++ S05T02N01FloresDelPozoJordi/src/main/java/cat/itacademy/barcelonactiva/floresdelpozo/jordi/s05/t02/n01/model/repository/PlayerRepository.java
@@ -0,0 +1,11 @@
+package cat.itacademy.barcelonactiva.floresdelpozo.jordi.s05.t02.n01.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cat.itacademy.barcelonactiva.floresdelpozo.jordi.s05.t02.n01.model.domain.Player;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Integer>{
+
+}
